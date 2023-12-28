7df9a9b2ff40705bb10b98e5971a33be858481d1
achandrawar <76238878+achandrawar@users.noreply.github.com>
2023-12-29 01:08:13 +0530

    Update App.java

--- Assignment 2/src/App.java
+++ Assignment 2/src/App.java
@@ -34,75 +34,12 @@ public class App {
                 case 8 :
                     System.exit(0);
                 default:
-                    break;
+                    System.out.println("Invalid Entry , Please try Code !");
             }
 
         }while(choice == 7);
         
-            
-
-        // String str="2001-01-01";  
-        // Date date=Date.valueOf(str);
-        // Student student = new Student(10, "Daniel", "Sakpal", date, "ramskpal@gmail.com", "44336345");
-        // Payment payment = new Payment(714,student,450.0,date);
-        // student.enrollInCourse(0,504,614);
-        // // student.updateStudentInfo("Ayush", "Chandrawar", date, "ayuchandrawar@gmail.com", "7582969935");
-        // // student.makePayment(500);
-        // //student.getEnrolledCourses();
-        // //student.displayStudentInfo(9);
-        // // student.getPaymentHistory();
-        // payment.getStudent(10);
-        // payment.getPaymentAmount();
-        // payment.getPaymentDate();
-        // System.out.println("\n\n---------------------------PAYMENT INFO-------------------------------\n\n");
-        // Enrollment enrollment = new Enrollment();
-        // enrollment.getStudent(602);
-        // System.out.println("\n\n---------------------------TEACHER INFO-------------------------------\n\n");
-        // Teacher teacher = new Teacher();
-        // teacher.displayTeacherInfo(101);
-
-        // System.out.println("\n\n---------------------------COURSE INFO-------------------------------\n\n");
-        // Course course = new Course(503,"", 0, 0);
-		// Teacher teacher1 = new Teacher(108,"","","","");   
-		// course.AssignTeacher(teacher1, 108);
-
-        //  //Method 2 UpdateCourseInfor method in course
-		// Course course2 = new Course(504,"Biology", 0, 0);
-		// course2.UpdateCourseInfo(504,"Biology");
-		// //course method 3 display-course-info
-		//   Teacher teacher2 = new Teacher(102,"","","","");
-		//  Course course3 = new Course(508,"Pscycology", 0, 0);
-		// course3.DisplayCourseInfo(teacher2);
-		
-		// //method 4 
-		//  Course course4 = new Course(508,"", 0, 0);
-		//  Teacher teacher3= new Teacher(508, null, null, null,null);
-		//  course4.GetEnrollments(teacher3);
-		 
-		//  //method 5
-		//  Course course5 = new Course(102,"", 0, 0);
-		//  Teacher teacher4= new Teacher(102, null, null, null,null);
-		//  course5.GetTeacher(teacher4);
-
-        // System.out.println("\n\n---------------------------Student Enrollment INFO-------------------------------\n\n");
-        // StudentEnrollment studentEnrollment = new StudentEnrollment();
-        // studentEnrollment.createNewStudent("John", "Doe", Date.valueOf("1995-08-15"),
-        //         "john.doe@example.com", "123-456-7890");
-
-        // System.out.println("\n\n---------------------------payment Enrollment INFO-------------------------------\n\n");
-        // PaymentRecord paymentRecord = new PaymentRecord();
-        // int studentId = 1;
-        // double paymentAmount = 500.00;
-        // paymentRecord.recordPayment(studentId, paymentAmount, date);
-
-        // System.out.println("\n\n---------------------------Enrollment Report INFO-------------------------------\n\n");
-        // EnrollmentReport enrollmentReport = new EnrollmentReport();
-
-        // // Example usage
-        // String courseName = "Biology";
-        // enrollmentReport.generateEnrollmentReport(courseName);
-
-        }
+    }
         public static void displayInfo(){
             System.out.println("1: Add a New Student");
             System.out.println("2: Assign Teacher To Course");
